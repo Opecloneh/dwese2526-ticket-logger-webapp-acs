0319d74e2f8025bc687870ea4d04d287c211ad32
Opeloneh <castilleroalvaro@gmail.com>
2026-01-13 12:33:46 +0100

    Data hecho en casa, subida de repositories

---
+++ src/main/java/org/iesalixar/daw2/acs/dwese2526_ticket_logger_webapp/repositories/UserRepository.java
@@ -0,0 +1,20 @@
+package org.iesalixar.daw2.acs.dwese2526_ticket_logger_webapp.repositories;
+
+import org.iesalixar.daw2.acs.dwese2526_ticket_logger_webapp.entities.Region;
+import org.iesalixar.daw2.acs.dwese2526_ticket_logger_webapp.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+/**
+ * Interfaz para operaciones CRUD sobre la entidad {@link User}.
+ * Define los métodos necesarios para listar, insertar, actualizar, eliminar y consultar usuarios.
+ */
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    boolean existsByEmail(String email);
+    boolean existsByEmailAndIdNot(String email, Long id);
+    Optional<User> findByEmail(String email);
+
+}
