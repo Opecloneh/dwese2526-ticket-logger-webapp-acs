3a76c6d8ba9800308fb1ebbc007d52e81487c334
Opeloneh <castilleroalvaro@gmail.com>
2026-01-13 13:40:00 +0100

    Commit de los repositories que no estaban subidos a la rama

---
+++ src/main/java/org/iesalixar/daw2/acs/dwese2526_ticket_logger_webapp/repositories/UserProfileRepository.java
@@ -0,0 +1,12 @@
+package org.iesalixar.daw2.acs.dwese2526_ticket_logger_webapp.repositories;
+
+import org.iesalixar.daw2.acs.dwese2526_ticket_logger_webapp.entities.Region;
+import org.iesalixar.daw2.acs.dwese2526_ticket_logger_webapp.entities.UserProfile;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserProfileRepository extends JpaRepository<UserProfile, Long> {
+    Optional<UserProfile> findByUserId(Long userId);
+    boolean existsByUserId(Long userId);
+}
